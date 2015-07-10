cbf2fc2d6e4cf7faa9583ae109f038bf7ab84bbf
Sam-Mauris Yong <mauris@hotmail.sg>
2015-07-10 15:41:38 +0800

    moved and updated files

--- index.js
+++ index.js
@@ -1,33 +1,104 @@
 var ipc = require('ipc');
+var nodeCrypto = require('crypto');
+var fs = require('fs');
 
 (function($){
+  var createDefaultCache = function () {
+    return {
+      "passwords": []
+    };
+  }
+
+  var CryptoService = function (algorithm, password) {
+    this.encrypt = function (text) {
+      var cipher = nodeCrypto.createCipher(algorithm, password);
+      var crypted = cipher.update(text, 'utf8', 'base64');
+      crypted += cipher.final('base64');
+      console.log(crypted);
+      return crypted;
+    };
+    this.decrypt = function (crypted) {
+      var decipher = nodeCrypto.createDecipher(algorithm, password);
+      var text = decipher.update(crypted, 'base64', 'utf8');
+      text += decipher.final('utf8');
+      return text;
+    };
+  }
+
+  var readCache = function(file, cryptoService, callback) {
+    callback = callback || function(){};
+    fs.access(file, fs.F_OK | fs.R_OK, function(err) {
+      if (err) {
+        // file does not existing or unreadable. try creating default cache
+        return callback(null, createDefaultCache());
+      } else {
+        fs.readFile(file, {"encoding": "utf8"}, function(err, data){
+          if (err) {
+            return callback(err);
+          }
+
+          var decrypted = cryptoService.decrypt(data);
+          var result = JSON.parse(decrypted);
+          return callback(null, result);
+        });
+      }
+    });
+  };
+
+  var writeCache = function(file, cryptoService, data, callback) {
+    callback = callback || function() {};
+    var crypted = cryptoService.encrypt(JSON.stringify(data));
+    fs.writeFile(file, crypted, {"encoding": "utf8"} , callback);
+  };
+
+  var generateRandomToken = function(length) {
+    var rnd = nodeCrypto.randomBytes(length);
+    var chars = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+    var len = chars.length;
+    var value = [];
+    for (var i = 0; i < length; ++i) {
+        value.push(chars[rnd[i] % len])
+    }
+    return value.join('');
+  };
+
+  var randomRange = function (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+  };
+
+  var createPassword = function(domain, minLength, maxLength) {
+    var length = randomRange(minLength, maxLength);
+    var password = generateRandomToken(length);
+    appData.passwords.push({
+      "domain": domain,
+      "password": password,
+      "created": Date.now
+    });
+  };
+
+  var algorithm = 'aes-256-cbc';
+  var masterPassword = 'testpassword';
+  var dataFile = __dirname + '/../keycache.dat';
+  var appData;
+  var cryptoService = new CryptoService(algorithm, masterPassword);
 
   $(function() {
-    var requestPasswordsTimer;
-
-    ipc.on('request-passwords-ready', function(arg) {
-      if (requestPasswordsTimer) {
-        window.clearInterval(requestPasswordsTimer);
-        requestPasswordsTimer = null;
-      }
-
-      console.log(arg);
-    });
-
-    ipc.on('create-password-success', function() {
-
-    });
 
     $("#btnAddPassword").click(function(){
-      ipc.send('create-password', {
-        "domain": $("#txtDomainName").val()
-      });
+      var domain = $("#txtDomainName").val().trim();
+      createPassword(domain, 14, 26);
+      writeCache(dataFile, cryptoService, appData);
       return false;
     });
 
-    requestPasswordsTimer = window.setInterval(function(){
-      ipc.send('request-passwords');
-    }, 500);
+    readCache(dataFile, cryptoService, function(err, data) {
+      appData = data;
+
+      $("#mainView").append()
+    });
+
+
+
   });
 
 })(jQuery);
